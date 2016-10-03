5e2e00284f38b24cafc6e418d13636ca195aae65
Dustin Hlava <dhlava@gmail.com>
2016-10-03 18:03:52 -0500

    style cleanup and updates

--- js/app.js
+++ js/app.js
@@ -1,40 +1,17 @@
-// $(document).ready(function(){
-	    	
-// 	var container = $('#portfolio_container');
-// 	container.imagesLoaded(function(){
-// 		container.masonry({
-// 			itemSelector : '.portfolio_item'
-// 		});
-// 	});
-		
-// 	$('.disabled').click(function(){
-// 		return false;
-// 	});
-	
-// 	$('.enlarge').live('click',function(){
-// 		$(this).next('.modal').modal({
-// 			overlayClose: true,
-// 			opacity:80
-// 			//persist: true
-// 		});	
-// 		return false;			
-// 	});
-	
-	
-// 	$('.no-video .video').click(function(){
-// 		$(this).removeClass('enlarge');
-// 		$(this).attr('href',$(this).attr('id'));
-	
-// 	});	
-// });
+$(document).ready(function(){
 
-// $(window).load(function() {
-//     if($('.flexslider').length > 0){
-// 	    $('.flexslider').flexslider({
-// 	    	animation: "slide",
-// 	    	randomize: true,
-// 	    	slideToStart: 1,
-// 	    	directionNav: true
-// 	    });
-//     }
-// });
+	$('.enlarge').on('click',function(e){
+		e.preventDefault();
+        $(this).next('.modal').modal({
+			overlayClose: true,
+			opacity:80
+		});	
+	});
+	
+	
+	$('.no-video .video').on('click', function(){
+		$(this).removeClass('enlarge');
+		$(this).attr('href',$(this).attr('id'));
+	
+	});	
+});
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(compression());
 app.use('/build', express.static(__dirname + '/build'));
 app.use('/images', express.static(__dirname + '/images'));
 app.use('/video', express.static(__dirname + '/video'));
-
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
