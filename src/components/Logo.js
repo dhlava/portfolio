51a337f7129b6c52ab36d249ebc10c50f5642217
Dustin Hlava <dhlava@gmail.com>
2017-12-04 02:32:34 -0600

    properly splits name for styling

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import './logo.css';
 
 class Logo extends Component {
+    
     render() {
         return (
             <div className="logo">
-                <h1>{this.props.name}</h1>
+                <h1>{this.props.name.split(' ')[0]}<span>{this.props.name.split(' ')[1]}</span></h1>
                 <h2>{this.props.label}</h2>
             </div>
         );
