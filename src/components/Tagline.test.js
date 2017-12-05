e2f09593564438631541c403183d0900a03d95b3
Dustin Hlava <dhlava@gmail.com>
2017-12-05 00:01:14 -0600

    removes tagline component that is no longer needed

--- src/components/Tagline.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Tagline from './Tagline';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Tagline />, div);
-});
