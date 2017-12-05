c5b34e21b7b223da5758464b6d3ac58c64703ee1
Dustin Hlava <dhlava@gmail.com>
2017-12-05 00:30:29 -0600

    adds additional content and styling to education section

--- src/components/EducationItem.js
+++ src/components/EducationItem.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import Moment from 'moment';
 
 class EducationItem extends Component {
     render() {
         return (
             <div className="resume_item education_item">
-                <h3>{this.props.item.institution}</h3>
+                <h3><a href="https://www.purdue.edu">{this.props.item.institution}</a></h3>
                 <div className="item_details">
-                    {this.props.item.studyType} in {this.props.item.area} &bull; {this.props.item.startDate} - {this.props.item.endDate}
-                    {this.props.item.gpa}
+                    <div className="item_details_title">{this.props.item.studyType} in {this.props.item.area}</div>
+                    {this.props.item.institutionLocation} &bull; {Moment(this.props.item.startDate).format("MMMM YYYY")} - {Moment(this.props.item.endDate).format("MMMM YYYY")}
+                    <ul className="highlight_list">
+                        <li className="highlight">{this.props.item.gpa}</li>
+                        <li className="highlight">{this.props.item.other}</li>
+                    </ul>
                 </div>
             </div>
         );
