aa635cbaf4462dd84c81d372034fe850de22b1ea
Dustin Hlava <dhlava@gmail.com>
2017-12-06 01:29:32 -0600

    removes default favicon, disables service worker

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './normalize.css';
 import 'font-awesome/css/font-awesome.min.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <App />, 
     document.getElementById('root')
 );
-registerServiceWorker();
+// registerServiceWorker();
