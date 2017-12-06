f6b9b7f153c4533505f38c0921db855b1b482f8e
Dustin Hlava <dhlava@gmail.com>
2017-12-06 01:30:57 -0600

    introduces responsive navigation menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import './navigation.css';
 
 class Navigation extends Component {
+    constructor(props){
+        super(props);
+        this.state = { isOpen: false };
+        this.toggleNavigation = this.toggleNavigation.bind(this);
+    }
+
+    toggleNavigation = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+          });
+    }
+
     render() {
         return (
             <nav className="main_navigation">
-                <ul>
-                    <li><a href="#about">About Me</a></li>
-                    <li><a href="#experience">What I've Done</a></li>
-                    <li><a href="#skills">What I Know</a></li>
-                    <li><a href="#education">Education</a></li>
-                    <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="/resume/Dustin_Hlava_Resume.pdf">Download PDF</a></li>
+                <button className="nav_toggle" onClick={this.toggleNavigation}><i className="fa fa-bars fa-3x" /></button>
+                <ul className={this.state.isOpen ? "nav_open": ""}>
+                    <li><a onClick={this.toggleNavigation} href="#about">About Me</a></li>
+                    <li><a onClick={this.toggleNavigation} href="#experience">What I've Done</a></li>
+                    <li><a onClick={this.toggleNavigation} href="#skills">What I Know</a></li>
+                    <li><a onClick={this.toggleNavigation} href="#education">Education</a></li>
+                    {/* <li><a onClick={this.toggleNavigation} href="#portfolio">Portfolio</a></li> */}
+                    <li><a onClick={this.toggleNavigation} href="resume/Dustin_Hlava_Resume.pdf">Download PDF</a></li>
                 </ul>
             </nav>
         );
