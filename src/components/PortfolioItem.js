676d632dcd74ddfd1d2b8d5ccbf94b29a0374737
Dustin Hlava <dhlava@gmail.com>
2017-12-08 02:20:43 -0600

    updates portfolio component with new layout and styling

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
 
-
-const RoleList = (props) => {
-    return (
-        <ul className="role_list">
-            {props.highlights.map( (highlight, index) =>
-                <li key={index}>{highlight}</li>
-            )}
-        </ul>
-    );
-};
-
 class PortfolioItem extends Component {
     render() {
         return (
-            <article className="portfolio_item">
-                <img src="images/portfolio/arrow.jpg" alt="Arrow Electronics Inc. - eCommerce" />
-                <div className="item_text">
-                    <h3><a href={this.props.item.url}>{this.props.item.name}</a></h3>
-                    <h4>{this.props.item.position}</h4>
-                    <p>{this.props.item.summary}</p>
-                    <RoleList highlights={this.props.item.highlights} />
+            <div className="portfolio_item_wrap">
+                <div className="portfolio_item">
+                    <img src={this.props.item.images[0].url} alt="Arrow Electronics Inc. - eCommerce" />
+                    <div className="item_overlay">
+                        <h3>{this.props.item.name}</h3>
+                        <p class="expand_text">View details</p>
+                    </div>
                 </div>
-            </article>
+            </div>
         );
     }
 }
