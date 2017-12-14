f1af55bd9974c7731fa97cf21a13af8c08717d47
Dustin Hlava <dhlava@gmail.com>
2017-12-14 01:28:10 -0600

    implements modal display for portfolio items, adds additional content to resume json

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
+import Modal from 'react-responsive-modal';
+import Button from './Button';
 
 class PortfolioItem extends Component {
+    constructor(){
+        super();
+        this.state = {
+            modalOpen: false
+        }
+
+        this.handleModalOpen = this.handleModalOpen.bind(this);
+        this.handleModalClose = this.handleModalClose.bind(this);
+    }
+
+    handleModalOpen(){
+        this.setState({modalOpen: true});
+    }
+
+    handleModalClose(){
+        this.setState({modalOpen: false});
+    }
+
     render() {
         return (
             <div className="portfolio_item_wrap">
-                <div className="portfolio_item">
-                    <img src={this.props.item.images[0].url} alt="Arrow Electronics Inc. - eCommerce" />
+                <div onClick={this.handleModalOpen} className="portfolio_item">
+                    <img src={this.props.item.images[0].url} alt={this.props.item.name} />
                     <div className="item_overlay">
                         <h3>{this.props.item.name}</h3>
-                        <p class="expand_text">View details</p>
+                        <p className="expand_text">View details</p>
                     </div>
                 </div>
+                <Modal showCloseIcon={false} classNames={{modal: 'portfolio_modal_container'}} open={this.state.modalOpen} onClose={this.handleModalClose}>
+                    <img src={this.props.item.images[1].url} alt="" />
+                    <div className="portfolio_description_content">
+                        <h3>{this.props.item.name}</h3>
+                        <h4>{this.props.item.type}</h4>
+                        <p>{this.props.item.description}</p>
+                    </div>
+                    <Button clickHandler={this.handleModalClose}>Close</Button>
+                </Modal>
             </div>
         );
     }
