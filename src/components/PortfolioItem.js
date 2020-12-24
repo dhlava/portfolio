b5917a2cbf21c0edbab5b2d1f70c5a558ddb04bd
Dustin Hlava <dhlava@gmail.com>
2020-12-23 22:26:30 -0600

    updating dependencies

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,48 +1,34 @@
-import React, { Component } from 'react';
-import Modal from 'react-responsive-modal';
+import React, { Component, useState } from 'react';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 import Button from './Button';
 
-class PortfolioItem extends Component {
-    constructor(){
-        super();
-        this.state = {
-            modalOpen: false
-        }
+const PortfolioItem = (props) => {
+    const [open, setOpen] = useState(false);
 
-        this.handleModalOpen = this.handleModalOpen.bind(this);
-        this.handleModalClose = this.handleModalClose.bind(this);
-    }
+    const onOpenModal = () => setOpen(true);
+    const onCloseModal = () => setOpen(false);
 
-    handleModalOpen(){
-        this.setState({modalOpen: true});
-    }
-
-    handleModalClose(){
-        this.setState({modalOpen: false});
-    }
-
-    render() {
-        return (
-            <div className="portfolio_item_wrap">
-                <div onClick={this.handleModalOpen} className="portfolio_item">
-                    <img src={this.props.item.images[0].url} alt={this.props.item.name} />
-                    <div className="item_overlay">
-                        <h3>{this.props.item.name}</h3>
-                        <p className="expand_text">View details</p>
-                    </div>
+    return (
+        <div className="portfolio_item_wrap">
+            <div onClick={onOpenModal} className="portfolio_item">
+                <img src={props.item.images[0].url} alt={props.item.name} />
+                <div className="item_overlay">
+                    <h3>{props.item.name}</h3>
+                    <p className="expand_text">View details</p>
                 </div>
-                <Modal little showCloseIcon={false} classNames={{modal: 'portfolio_modal_container'}} open={this.state.modalOpen} onClose={this.handleModalClose}>
-                    <img src={this.props.item.images[1].url} alt="" />
-                    <div className="portfolio_description_content">
-                        <h3>{this.props.item.name}</h3>
-                        <h4>{this.props.item.type}</h4>
-                        <p>{this.props.item.description}</p>
-                    </div>
-                    <Button clickHandler={this.handleModalClose}>Close</Button>
-                </Modal>
             </div>
-        );
-    }
+            <Modal little showCloseIcon={false} classNames={{modal: 'portfolio_modal_container'}} open={open} onClose={onCloseModal}>
+                <img src={props.item.images[1].url} alt="" />
+                <div className="portfolio_description_content">
+                    <h3>{props.item.name}</h3>
+                    <h4>{props.item.type}</h4>
+                    <p>{props.item.description}</p>
+                </div>
+                <Button clickHandler={onCloseModal}>Close</Button>
+            </Modal>
+        </div>
+    );
 }
 
 export default PortfolioItem;
